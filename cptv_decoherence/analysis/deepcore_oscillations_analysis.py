4d1d66f3c3ef4165168b0d6a795b561fae7fd976
Tom Stuttard <stuttard@icecube.wisc.edu>
2023-04-19 14:13:35 +0200

    Getting DeepCore oscillations analysis running

--- cptv_decoherence/analysis/deepcore_oscillations_analysis.py
+++ cptv_decoherence/analysis/deepcore_oscillations_analysis.py
@@ -14,267 +14,179 @@ from pisa.core.pipeline import Pipeline
 from pisa.analysis.analysis import Analysis
 from pisa import FTYPE, ureg
 
+from deimos.utils.plotting import *
 
-# ## Model
+#
+# Globals
+#
+
+METRIC = "chi2"
+
+
+#
+# Load model (e.g. the MC expectation for our data)
+#
 
 # We can now instantiate the `model` (given our configs) that we later fit to data. This now containes two `Pipelines` in a `DistributionMaker`, one for our neutrinos, and one for the background muons.
-
-# In[24]:
-
-
-model = DistributionMaker(["settings/pipeline/IceCube_3y_neutrinos.cfg", "settings/pipeline/IceCube_3y_muons.cfg"])
-# this just turns on profiling
-model.profile = True
-model
-
-
-# Our model has a number of free parameters, that will be used in our fit to the data
-
-# In[25]:
-
-
-model.params.free
-
+# template_maker = DistributionMaker(["settings/pipeline/IceCube_3y_neutrinos.cfg", "settings/pipeline/IceCube_3y_muons.cfg"])
+template_maker = DistributionMaker(["cptv_decoherence/settings/pipeline_deepcore_nu_mc.cfg", "cptv_decoherence/settings/pipeline_deepcore_mu_mc.cfg"])
+print(template_maker)
 
 # The two pipelines are quite different, with most complexity in the neutrino pipeline, that has several `Stage`s and free parameters:
-
-# In[26]:
-
-
-model.pipelines[0]
-
-
-# In[27]:
-
-
-model.pipelines[0].stages[2].params
-
+print(template_maker.pipelines[0])
 
 # While the muon pipleine is rather simple
-
-# In[28]:
-
-
-model.pipelines[1]
+print(template_maker.pipelines[1])
 
 
-# ## Retrieve Outputs
+#
+# Choose free parameters
+#
+
+# The templare aker has a number of model parameters, both physics and nuisance parameters
+# Choose which to use here
+params_to_use = [
+    # Flux
+    # 'nue_numu_ratio',
+    # 'Barr_uphor_ratio',
+    # 'Barr_nu_nubar_ratio',
+    'delta_index',
+    # Oscillations
+    # 'theta13',
+    'theta23',
+    'deltam31',
+    # Norm
+    'aeff_scale',
+    # 'nu_nc_norm',
+    # Atmospheric muon background
+    # 'atm_muon_scale',
+    # Detector systematics
+    # "opt_eff_overall",
+    # "opt_eff_lateral",
+    # "opt_eff_headon",
+    # "ice_scattering",
+    # "ice_absorption",
+]
+
+# Now fix/free these params in the model
+for pipeline in template_maker.pipelines :
+    for param in pipeline.params :
+        if param.name in params_to_use :
+            param.is_fixed = False
+        else :
+            param.is_fixed = True
+
+print("\nFree params :")
+for p in template_maker.params.free :
+    print("  %s" % p.name)
+print("")
+
+
+#
+# Get analysis histograms
 # 
+
 # We can get individual outputs from just a pipleine like so. This fetches outputs from the neutrino pipleine, which are 12 maps.
+maps = template_maker.get_outputs()[0] #TODO This is neutrinos, also get muons
 
-# In[29]:
-
-
-maps = model.pipelines[0].get_outputs()
-
-
-# In[30]:
-
-
-maps.names
-
-
-# In[31]:
-
-
+# Plot these histograms
 fig, axes = plt.subplots(3,4, figsize=(24,10))
 plt.subplots_adjust(hspace=0.5)
 axes = axes.flatten()
-
 for m, ax in zip(maps, axes):
     m.plot(ax=ax)
+fig.tight_layout()
+
+# Also plot the total expectation for the full model (all neutrinos + muons):
+template_maker.get_outputs(return_sum=True).plot()
 
 
-# If we are interested in just the total expecatation from the full model (all neutrinos + muons), we can do the following:
+#
+# Load real detector data
+#
 
-# In[32]:
-
-
-model.get_outputs(return_sum=True).plot()
-
-
-# ## Diff plots
-# 
-# Let's explore how a change in one of our nuisance parameters affects the expected counts per bin. Here we choose a *hole ice* parameter and move it a smidge.
-
-# In[33]:
-
-
-# reset all free parameters to put them back to nominal values
-model.reset_free()
-nominal = model.get_outputs(return_sum=True)
-
-# shift one parameter
-model.params.opt_eff_lateral.value = 20
-sys = model.get_outputs(return_sum=True)
-
-
-# In[34]:
-
-
-((nominal[0] - sys[0])/nominal[0]).plot(symm=True, clabel="rel. difference")
-
-
-# ## Get Data
-# 
 # We can load the real observed data too. This is a Pipeline with no free parameters, as the data is of course fixed.
 # NB: When developping a new analysis you will **not** be allowed to look at the data as we do here before the box opening (c.f. *blindness*).
 
-# In[35]:
+# Load the data pipeline
+# data_maker = Pipeline("settings/pipeline/IceCube_3y_data.cfg")
+data_maker = Pipeline("cptv_decoherence/settings/pipeline_deepcore_data.cfg")
+print(data_maker)
 
-
-# real data
-data_maker = Pipeline("settings/pipeline/IceCube_3y_data.cfg")
+# Run it to get the data histogram
 data = data_maker.get_outputs()
 
-
-# In[36]:
-
-
-data_maker
-
-
-# In[37]:
-
-
+# Plot data, and data-MC comparison
 fig, ax = plt.subplots(1, 3, figsize=(20, 3))
-
-model.reset_free()
-nominal = model.get_outputs(return_sum=True)
-
+template_maker.reset_free()
+nominal = template_maker.get_outputs(return_sum=True)
 data.plot(ax=ax[0], title="Data")
 nominal.plot(ax=ax[1], title="Model")
 (data - nominal).plot(ax=ax[2], symm=True, title="Diff")
+fig.tight_layout()
 
 
-# ## Fitting
-# 
+
+#
+# Fitting
+#
+ 
 # For fitting we need to configure a minimizer, several standard cfgs are available, but you can also define your own.
 # For the fit we need to choose a `metric`, and by default, theta23 octants, which are quasi degenerate, are fit seperately, which means two fits are run.
 
-# In[39]:
+# Define minimizer settings
+# minimizer_cfg = pisa.utils.fileio.from_file('settings/minimizer/slsqp_ftol1e-6_eps1e-4_maxiter1000.json')
+minimizer_cfg = pisa.utils.fileio.from_file('cptv_decoherence/settings/minimizer.json')
 
 
-minimizer_cfg = pisa.utils.fileio.from_file('settings/minimizer/slsqp_ftol1e-6_eps1e-4_maxiter1000.json')
+# Create analysis class
 ana = Analysis()
 
+print("\nRun fit....")
+result = ana.fit_hypo(
+    data,
+    template_maker,
+    metric=METRIC,
+    minimizer_settings=minimizer_cfg,
+    fit_octants_separately=True, # This fits both theta23 octants separately and chooses the best
+)
 
-# In[41]:
+
+#
+# Store results
+#
+
+# Save the fit results to a file
+results_file = os.path.abspath("results.json")
+pisa.utils.fileio.to_file(result, results_file)
 
 
-get_ipython().run_cell_magic('time', '', "result = ana.fit_hypo(\n         data,\n         model,\n         metric='mod_chi2',\n         minimizer_settings=minimizer_cfg,\n         fit_octants_separately=True,\n        )")
-
+#
+# Check results
+#
 
 # Here we can view the bestfit parameters - the result of our fit.
 # We have run two fits (separately for each theta23 octant), and the best result is stored in `results[0]` (both fits are also available under `results[1]`)
 
-# In[18]:
-
-
+# Grab fit results
 bestfit_params = result[0]['params'].free
-bestfit_params
+print("\nFit results :")
+for p in bestfit_params :
+    print("  %s = %s (nominal = %s)" % (p.name, p.value, p.nominal_value))
+print("  %s = %s" % (result[0]["metric"], result[0]["metric_val"]))
+print("")
+
+# Plot best fit histogram
+#TODO
 
 
-# In[19]:
+#
+# Done
+#
 
+# Report
+print("\nFit results : %s" % results_file)
 
-# update the model with the bestfit (make a copy here, because we don't want our bestfit params to be affected (NB: stuff is passed by reference in python))
-model.update_params(copy.deepcopy(bestfit_params))
-
-
-# Let's see how good that fit looks like. We here construct signed mod_chi2 maps by hand.
-# You can see that after the fit, it improved considerably, and the distribution of chi2 values is now more uniform - not much features can be seen anymore.
-
-# In[20]:
-
-
-fig, ax = plt.subplots(2, 3, figsize=(20, 7))
-plt.subplots_adjust(hspace=0.5)
-
-bestfit = model.get_outputs(return_sum=True)
-
-data.plot(ax=ax[0,0], title="Data")
-nominal.plot(ax=ax[0,1], title="Nominal")
-diff = data - nominal
-(abs(diff)*diff/(nominal + unp.std_devs(nominal.hist['total']))).plot(ax=ax[0,2], symm=True, title=r"signed $\chi^2$", vmin=-12, vmax=12)
-
-data.plot(ax=ax[1,0], title="Data")
-bestfit.plot(ax=ax[1,1], title="Bestfit")
-diff = data - bestfit
-(abs(diff)*diff/(bestfit + unp.std_devs(bestfit.hist['total']))).plot(ax=ax[1,2], symm=True, title=r"signed $\chi^2$", vmin=-12, vmax=12)
-
-
-# When checking the chi2 value from the fitted model, you maybe see that it is around 113, while in the minimizer loop we saw it converged to 116. It is important to keep in mind that in the fit we had extended the metric with prior penalty terms. When we add those back we get the identical number as reported in the fit.
-
-# In[21]:
-
-
-print(data.metric_total(nominal, 'mod_chi2'))
-print(data.metric_total(bestfit, 'mod_chi2'))
-
-
-# Evaluating other metrics just for fun:
-
-# In[22]:
-
-
-for metric in pisa.utils.stats.ALL_METRICS:
-    try:
-        print('%s = %.3f'%(metric,data.metric_total(bestfit, metric)))
-    except:
-        print('%s failed'%metric)
-
-
-# Adding prior penalty terms
-
-# In[23]:
-
-
-model.update_params(copy.deepcopy(bestfit_params))
-data.metric_total(bestfit, 'mod_chi2') + model.params.priors_penalty('mod_chi2')
-
-
-# In[24]:
-
-
-result[0]['metric_val']
-
-
-# ## Storing the results to a file
-# 
-# Since the fit took a while, it might be useful to store the results to a file. (NB: in this example we use a temp file, but in real life you would of course just use a real pathname instead!)
-
-# In[25]:
-
-
-import tempfile
-temp = tempfile.NamedTemporaryFile(suffix='.json')
-pisa.utils.fileio.to_file(result, temp.name)
-
-
-# To reload, we can read the file. But to get PISA objects back, they need to instantiated
-
-# In[26]:
-
-
-result_reload = pisa.utils.fileio.from_file(temp.name)
-bestfit_params = pisa.core.ParamSet(result_reload[0]['params']).free
-bestfit_params
-temp.close()
-
-
-# ## Profiling
-# 
-# To understand what parts of the model were executed how many times, and how long it took, the profiling  can help:
-
-# In[27]:
-
-
-model.pipelines[0].report_profile()
-
-
-# In[ ]:
-
-
-
-
+# Dump plots
+print("")
+dump_figures_to_pdf( __file__.replace(".py", ".pdf") )
